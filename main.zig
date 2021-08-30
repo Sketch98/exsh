781f54fd3d8837d73a693e07a948a1a0d4b830db
Nathan Sketch <sketchn98@gmail.com>
2021-08-30 07:56:08 -0400

    added triev search

--- main.zig
+++ main.zig
@@ -6,54 +6,92 @@ pub fn main() anyerror!void {
     comptime var i = 0;
     var kids: [32]*Triev = undefined;
     inline while (i < kids.len) : (i += 1) {
-        kids[i] = &Triev{ .bit_string = i * i, .kids = empty_arr[0..], .val = kid_msg[0..] };
+        kids[i] = &Triev{ .bit_string = 0, .kids = empty_arr[0..], .val = kid_msg[0..] };
     }
+    var grandkids: [1]*Triev = undefined;
+    var grandkid_msg: [16]u8 = "a wee little bab".*;
+    grandkids[0] = &Triev{ .bit_string = 23, .kids = empty_arr[0..], .val = grandkid_msg[0..] };
+    kids[5].bit_string = 0x0000_0200;
+    kids[5].kids = grandkids[0..];
     var root_msg: [36]u8 = "all your permission are belong to us".*;
-    const triev = Triev{ .bit_string = 0xFFFF_FFFF, .kids = kids[0..], .val = root_msg[0..] };
-    std.debug.print("{} {s}\n", .{ triev.bit_string, triev.val });
-    for (triev.kids) |kid| {
+    var root = Triev{ .bit_string = 0xFFFFFFFF, .kids = kids[0..], .val = root_msg[0..] };
+    std.debug.print("{} {s}\n", .{ root.bit_string, root.val });
+    for (root.kids) |kid| {
         std.debug.print("{} {s}\n", .{ kid.bit_string, kid.val });
     }
+    var key: [3]u8 = "Zig".*;
+    _ = try root.get(key[0..]);
+
     var args = [_:null]?[*:0]const u8{ "echo", "yo", "dawg", "i heard" };
     //const envp = [0:null]?[*:0]const u8{};
     return std.os.execvpeZ(args[0].?, &args, &[0:null]?[*:0]const u8{});
 }
 
 // triev as in retrieval. much better alternative to the ambiguous trie imo.
-const Triev = struct { bit_string: u32 = 0, kids: []*Triev, val: []u8 };
+const Triev = struct {
+    bit_string: u32 = 0,
+    kids: []*Triev,
+    val: []u8,
+    // get subtriev at key position. if subtriev doesn't exist, return null
+    fn get(self: *Triev, key: []u8) TrievError!?*Triev {
+        var cur = self;
+        var i: u8 = 0;
+        while (i < key.len) : (i += 1) {
+            std.debug.print("{s}\n", .{cur.val});
+            if (cur.kids.len == 0)
+                return null;
+            const k = try compress_key(key[i]);
+            std.debug.print("{s} {}\n", .{ "looking for", k });
+            const bit_flag = @as(u32, 1) << k;
+            // assuming very sparse triev, only having one kid is most common
+            // and i'm already computing bit_flag for the general case
+            if (cur.bit_string == bit_flag) {
+                std.debug.print("{s}\n", .{"an only child"});
+                cur = cur.kids[0];
+            } else if (cur.bit_string & bit_flag != 0) {
+                std.debug.print("{s}\n", .{"it exists! now what?"});
+                // -1 because we want array index which is 0-based
+                const index = hamming_weight(cur.bit_string >> k) - 1;
+                cur = cur.kids[index];
+                std.debug.print("{s} {}\n", .{ "HAMMMMMM", index });
+            } else {
+                std.debug.print("{s}\n", .{"i don't see nothin'"});
+                return null;
+            }
+        }
+        return cur;
+    }
+};
 
 const TrievError = error{
     InvalidKeyByte, //key must only be made of A-Za-Z_
-    KeyTooLong,
 };
-const max_key_len = 256;
 
 // squishes A-Za-z_ ranging 0x41-0x7A (64 bits) into 0x01-0x1F (32 bits)
-fn compress_key(key: []u8) TrievError!void {
-    if (key.len > max_key_len)
-        return TrievError.KeyTooLong;
-    for (key) |char, i| {
-        if (char == '_' or (char >= 'A' and char <= 'Z') or (char >= 'a' and char <= 'z')) {
-            key[i] &= 0x1F;
-        } else {
-            return TrievError.InvalidKeyByte;
-        }
-    }
+fn compress_key(key: u8) TrievError!u5 {
+    if (key == '_' or (key >= 'A' and key <= 'Z') or (key >= 'a' and key <= 'z'))
+        return @intCast(u5, key & 0x1F);
+    return TrievError.InvalidKeyByte;
+}
+
+// if only i could just use the x86 instruction but alas
+fn hamming_weight(bit_string: u32) u5 {
+    var bits = bit_string;
+    bits -= (bits >> 1) & 0x55555555;
+    bits = (bits & 0x33333333) + ((bits >> 2) & 0x33333333);
+    bits = (bits + (bits >> 4)) & 0x0f0f0f0f;
+    bits *%= 0x01010101;
+    return @intCast(u5, bits >> 24);
 }
 
 const expect = std.testing.expect;
 
 test "compress" {
     var automatic: [13]u8 = "Charlie_Daisy".*;
-    const manual = [_]u8{ 0x03, 0x08, 0x01, 0x12, 0x0C, 0x09, 0x05, 0x1F, 0x04, 0x01, 0x09, 0x13, 0x19 };
-    compress_key(&automatic) catch unreachable;
+    const manual = [_]u5{ 0x03, 0x08, 0x01, 0x12, 0x0C, 0x09, 0x05, 0x1F, 0x04, 0x01, 0x09, 0x13, 0x19 };
     for (automatic) |char, i| {
-        try expect(char == manual[i]);
+        try expect(compress_key(automatic(char)) == manual[i]);
     }
-    var bad_key = [_]u8{ 0x00, 0x07, 0x15 };
-    compress_key(&bad_key) catch |err|
+    compress_key(0x15) catch |err|
         try expect(err == TrievError.InvalidKeyByte);
-    var long_key = [_]u8{ 0x5A, 0x69, 0x67 } ** 100;
-    compress_key(&long_key) catch |err|
-        try expect(err == TrievError.KeyTooLong);
 }
