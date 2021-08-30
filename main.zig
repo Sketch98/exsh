3ba36475043326ee85dbe09b1ce22dcdbf9a7f8d
Nathan Sketch <sketchn98@gmail.com>
2021-08-30 02:56:45 -0400

    add values to triev

--- main.zig
+++ main.zig
@@ -1,23 +1,26 @@
 const std = @import("std");
 
-// triev as in retrieval. much better alternative to the ambiguous trie imo.
-const Triev = struct { bit_string: u32 = 0, kids: []*Triev };
-
 pub fn main() anyerror!void {
     var empty_arr = [0]*Triev{};
+    var kid_msg: [25]u8 = "just some snot-nosed brat".*;
     comptime var i = 0;
     var kids: [32]*Triev = undefined;
     inline while (i < kids.len) : (i += 1) {
-        kids[i] = &Triev{ .bit_string = i * i, .kids = empty_arr[0..] };
+        kids[i] = &Triev{ .bit_string = i * i, .kids = empty_arr[0..], .val = kid_msg[0..] };
     }
-    const triev = Triev{ .bit_string = 0xFFFF_FFFF, .kids = kids[0..] };
+    var root_msg: [36]u8 = "all your permission are belong to us".*;
+    const triev = Triev{ .bit_string = 0xFFFF_FFFF, .kids = kids[0..], .val = root_msg[0..] };
+    std.debug.print("{} {s}\n", .{ triev.bit_string, triev.val });
     for (triev.kids) |kid| {
-        std.debug.print("{} {any}\n", .{ kid.bit_string, kid.kids });
+        std.debug.print("{} {s}\n", .{ kid.bit_string, kid.val });
     }
     var args = [_:null]?[*:0]const u8{ "echo", "yo", "dawg", "i heard" };
     //const envp = [0:null]?[*:0]const u8{};
     return std.os.execvpeZ(args[0].?, &args, &[0:null]?[*:0]const u8{});
 }
+
+// triev as in retrieval. much better alternative to the ambiguous trie imo.
+const Triev = struct { bit_string: u32 = 0, kids: []*Triev, val: []u8 };
 
 const TrievError = error{
     InvalidKeyByte, //key must only be made of A-Za-Z_
