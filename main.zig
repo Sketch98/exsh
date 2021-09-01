5d3b994c50f09122770ea95586717bc7ed8d9a3c
Nathan Sketch <sketchn98@gmail.com>
2021-09-01 10:19:31 -0400

    separate key check from squish. i could've just put the check in get, but i'll probably need it for parsing.

--- main.zig
+++ main.zig
@@ -39,13 +39,15 @@ const Triev = struct {
     val: []const u8,
     // walk toward key and return pointer to furthest triev reached
     fn get(self: *Triev, key: []const u8) TrievError!*Triev {
+        if (!is_valid_key(key))
+            return TrievError.InvalidKey;
         var cur = self;
         var i: u8 = 0;
         while (i < key.len) : (i += 1) {
             if (cur.bit_string == 0)
                 break;
             // std.debug.print("{} {s}\n", .{ cur.depth, cur.val });
-            const k = try compress_key(key[i]);
+            const k = @intCast(u5, key[i] & 0x1F);
             // std.debug.print("\t{s} {}\n", .{ "looking for", k });
             const bit_flag = @as(u32, 1) << k;
             // assuming very sparse triev, only having one kid is most common
@@ -86,7 +88,7 @@ const Triev = struct {
                 trievs[i].depth = t.depth + i + 1;
                 if (i == ikey.len - 1)
                     break;
-                trievs[i].bit_string = one << try compress_key(ikey[i + 1]);
+                trievs[i].bit_string = one << @intCast(u5, ikey[i + 1] & 0x1F);
                 trievs[i].val = empty_str[0..];
                 trievs[i].kids = pointers[i][0..];
                 pointers[i][0] = &trievs[i + 1];
@@ -98,14 +100,14 @@ const Triev = struct {
             if (t.bit_string == 0) {
                 const p = try a.create([1]*Triev);
                 p[0] = &trievs[0];
-                t.bit_string = one << try compress_key(ikey[0]);
+                t.bit_string = one << @intCast(u5, ikey[0] & 0x1F);
                 t.kids = p;
             } else {
                 const p = try a.alloc(*Triev, t.kids.len + 1);
-                const bit = try compress_key(ikey[0]);
-                const bit_flag = one << bit;
+                const bit_pos = @intCast(u5, ikey[0] & 0x1F);
+                const bit_flag = one << bit_pos;
                 t.bit_string |= bit_flag;
-                const index = hamming_weight((t.bit_string & ~bit_flag) << 31 - bit);
+                const index = hamming_weight((t.bit_string & ~bit_flag) << 31 - bit_pos);
                 std.mem.copy(*Triev, p[0..index], t.kids[0..index]);
                 p[index] = &trievs[0];
                 std.mem.copy(*Triev, p[index + 1 ..], t.kids[index..]);
@@ -133,6 +135,8 @@ const Triev = struct {
         while (true) {
             if (cur.val.len != 0) {
                 std.debug.print("key ", .{});
+                // | 0x40 to unsquish key. i'd prefer to print in lowercase
+                // but i really can't be bothered to write more than an or.
                 for (stack.items) |item| std.debug.print("{c}", .{@intCast(u8, item.bit_shift) | 0x40});
                 std.debug.print("\n\tval {s}\n", .{cur.val});
             }
@@ -166,15 +170,8 @@ const Triev = struct {
 };
 
 const TrievError = error{
-    InvalidKeyByte, //key must only be made of A-Za-Z_
+    InvalidKey, //key must only be made of A-Za-Z_
 };
-
-// squishes A-Za-z_ ranging 0x41-0x7A (64 bits) into 0x01-0x1F (32 bits)
-fn compress_key(key: u8) TrievError!u5 {
-    if (key == '_' or (key >= 'A' and key <= 'Z') or (key >= 'a' and key <= 'z'))
-        return @intCast(u5, key & 0x1F);
-    return TrievError.InvalidKeyByte;
-}
 
 // get number of 1's in bit string
 // can return u5 because input is guaranteed to not be 0xFFFF_FFFFF
@@ -199,15 +196,24 @@ fn next_bit(bit_string: u32, bit_shift: u5) u5 {
     return shift;
 }
 
+// checks that key is only A-Za-z_ so it can safely be squished by @intCast
+fn is_valid_key(key: []const u8) bool {
+    for (key) |k|
+        if (k != '_' and (k < 'A' or k > 'Z') and (k < 'a' or k > 'z'))
+            return false;
+    return true;
+}
+
 const expect = std.testing.expect;
 
 test "compress" {
     var automatic: [13]u8 = "Charlie_Daisy".*;
     const manual = [_]u5{ 0x03, 0x08, 0x01, 0x12, 0x0C, 0x09, 0x05, 0x1F, 0x04, 0x01, 0x09, 0x13, 0x19 };
     for (automatic) |char, i| {
-        const c = try compress_key(char);
+        const c = @intCast(u5, char & 0x1F);
         try expect(c == manual[i]);
     }
-    _ = compress_key(0x15) catch |err|
-        try expect(err == TrievError.InvalidKeyByte);
+    try expect(is_valid_key(automatic[0..]));
+    automatic[7] = '~';
+    try expect(!is_valid_key(automatic[0..]));
 }
